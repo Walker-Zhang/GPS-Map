5b5fa25075d1073ac0c772e754b0067754ad542d
qianfeng <apple@aofel.local>
2014-04-18 09:22:21 +0800

    Initial Commit

---
+++ GPS&Map/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GPS&Map
+//
+//  Created by qianfeng on 14-4-18.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GPS&MapTests/GPS_MapTests.m
@@ -0,0 +1,34 @@
+//
+//  GPS_MapTests.m
+//  GPS&MapTests
+//
+//  Created by qianfeng on 14-4-18.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GPS_MapTests : XCTestCase
+
+@end
+
+@implementation GPS_MapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
